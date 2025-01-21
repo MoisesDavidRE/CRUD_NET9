45a773abcd19ea60a0d2f0ef5a5e5e544a11a73a
Liz Perdomo <129300353+Lizperdomo@users.noreply.github.com>
2025-01-20 18:46:38 -0600

    Add files via upload

---
+++ Services/IUsuarioService.cs
@@ -0,0 +1,12 @@
+﻿using CompartirDatos;
+namespace Blazor_CRUD.Services
+{
+    public interface IUsuarioService
+    {
+        Task<List<Usuario>> Lista();
+        Task<Usuario> Buscar (int id);
+        Task<int> Guardar(Usuario usr);
+        Task<int> Editar(Usuario usr);
+        Task<bool> Eliminar(int id);
+    }
+}
