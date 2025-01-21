db8168057ec86a2ace939687ee5cc2f6279883bb
Liz Perdomo <129300353+Lizperdomo@users.noreply.github.com>
2025-01-20 18:49:29 -0600

    Add files via upload

---
+++ APIBlazor/Models/AsistenciasDbContext.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql.Scaffolding.Internal;
+
+namespace APIBlazor.Models;
+
+public partial class AsistenciasDbContext : DbContext
+{
+    public AsistenciasDbContext()
+    {
+    }
+
+    public AsistenciasDbContext(DbContextOptions<AsistenciasDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Asistencia> Asistencias { get; set; }
+
+    public virtual DbSet<Usuario> Usuarios { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .UseCollation("utf8mb4_general_ci")
+            .HasCharSet("utf8mb4");
+
+        modelBuilder.Entity<Asistencia>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("asistencias");
+
+            entity.HasIndex(e => e.UsuarioId, "usuario_id");
+
+            entity.Property(e => e.Id)
+                .HasColumnType("int(11)")
+                .HasColumnName("id");
+            entity.Property(e => e.Fecha).HasColumnName("fecha");
+            entity.Property(e => e.Presente)
+                .HasDefaultValueSql("'0'")
+                .HasColumnName("presente");
+            entity.Property(e => e.UsuarioId)
+                .HasColumnType("int(11)")
+                .HasColumnName("usuario_id");
+        });
+        modelBuilder.Entity<Usuario>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PRIMARY");
+
+            entity.ToTable("usuarios");
+
+            entity.HasIndex(e => e.Correo, "correo").IsUnique();
+
+            entity.Property(e => e.Id)
+                .HasColumnType("int(11)")
+                .HasColumnName("id");
+            entity.Property(e => e.Contrasena)
+                .HasMaxLength(255)
+                .HasColumnName("contrasena");
+            entity.Property(e => e.Correo)
+                .HasMaxLength(150)
+                .HasColumnName("correo");
+            entity.Property(e => e.FechaRegistro)
+                .HasDefaultValueSql("current_timestamp()")
+                .HasColumnType("timestamp")
+                .HasColumnName("fecha_registro");
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(100)
+                .HasColumnName("nombre");
+            entity.Property(e => e.Rol)
+                .HasDefaultValueSql("'usuario'")
+                .HasColumnType("enum('usuario','admin')")
+                .HasColumnName("rol");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
